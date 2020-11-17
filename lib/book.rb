fd7a6f164b987658ef410595aa20514dfd5f9c5a
Akram_Alam <arundelfootball23@gmail.com>
2020-11-17 17:27:36 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book 
 
+   attr_reader :title
+   attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+    # def title  == getter method for title attr_reader
+    #     @title
+    # end 
+    # def author=(author) == attr_writer/setter method
+    #     @author = author 
+    # end 
+
+    # def author 
+    #     @author 
+    # end 
+end 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe 
+
+    attr_reader :brand 
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    def cobble 
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    
+    end 
+    # def color=(color)
+    #     @color = color
+    # end 
+
+    # def color 
+    #     @color
+    # end 
+
+end 
